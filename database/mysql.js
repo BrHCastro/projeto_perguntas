736a68a836646d04359bab7a1c68d8088f27f552
brhcastro <hendecastro@gmail.com>
2021-09-07 17:01:32 -0300

    Criado variavens de ambiente

--- database/mysql.js
+++ database/mysql.js
@@ -1,7 +1,11 @@
 const Sequelize = require('sequelize');
+require('dot-env');
 
-const connection = new Sequelize('whats_db', 'root', '', {
-    host: 'localhost',
+const connection = new Sequelize(
+    process.env.DB_DATABASE, 
+    process.env.DB_USER,
+    process.env.DB_PASS, {
+    host: process.env.DB_HOST,
     dialect: 'mysql'
 });
 
